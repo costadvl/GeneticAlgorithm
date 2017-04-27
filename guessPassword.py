92ed0fae9a72e6d5a7a99451e8147be9fa91ded9
costa <costadvl@gmail.com>
2017-04-27 22:05:13 +0200

    previous to unitest

--- guessPassword.py
+++ guessPassword.py
@@ -1,48 +1,37 @@
-# Author: Clinton Sheppard <fluentcoder@gmail.com>
-# Repository: https://repl.it/CZL1/1
-# Copyright (c) 2016 Clinton Sheppard
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.  See the License for the specific language governing
-# permissions and limitations under the License.
-
-import random
+import genetic
 import datetime
+import unittest
 
 
-def get_fitness(guess):
+def get_fitness(guess, target):
     return sum(1 for expected, actual in zip(target, guess)
                if expected == actual)
 
 
-def display(guess):
+def display(genes, target, startTime):
     timeDiff = datetime.datetime.now() - startTime
-    fitness = get_fitness(guess)
-    print("{0}\t{1}\t{2}".format(guess, fitness, str(timeDiff)))
+    fitness = get_fitness(genes, target)
+    print("{0}\t{1}\t{2}".format(genes, fitness, str(timeDiff)))
 
 
-random.seed()
-geneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
-target = "Hello World!"
-startTime = datetime.datetime.now()
-bestParent = generate_parent(len(target))
-bestFitness = get_fitness(bestParent)
-display(bestParent)
-while True:
-    child = mutate(bestParent)
-    childFitness = get_fitness(child)
+def test_Hello_World():
+    target = "Hello World!"
+    guess_password(target)
 
-    if bestFitness >= childFitness:
-        continue
-    display(child)
-    if childFitness >= len(bestParent):
-        break
-    bestFitness = childFitness
-    bestParent = child
+
+def guess_password(target):
+    geneset = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!."
+    startTime = datetime.datetime.now()
+
+    def fnGetFitness(genes):
+        return get_fitness(genes, target)
+
+    def fnDisplay(genes):
+        display(genes, target, startTime)
+
+    optimalFitness = len(target)
+    genetic.get_best(fnGetFitness, len(target), optimalFitness, geneset, fnDisplay)
+
+
+if __name__ == '__main__':
+    test_Hello_World()
